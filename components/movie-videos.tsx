e9c9843c986bb7dab8acabb066d64ae2012cb7af
jh_p <jhpark@oceanui.dooray.com>
2024-02-02 16:01:32 +0900

    0202_2

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,18 +1,18 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-videos.module.css';
 
 async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error('에러닷');
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVidoes({ id }: { id: string }) {
   const videos = await getVideos(id);
   return (
-    <div>
-      <h6>{JSON.stringify(videos)}</h6>
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe key={video.id} src={`https://youtube.com/embed/${video.key}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={video.name} />
+      ))}
     </div>
   );
 }
